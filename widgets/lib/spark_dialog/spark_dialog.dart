91b348ba803def904c8a1f017140d21329a9ab65
Sergey Shevchenko <sergeygs@google.com>
2014-05-06 03:42:58 -0700

    Dynamic buttons (initilized fine, but don't render correctly)

--- widgets/lib/spark_dialog/spark_dialog.dart
+++ widgets/lib/spark_dialog/spark_dialog.dart
@@ -9,10 +9,70 @@ import 'package:polymer/polymer.dart';
 import '../spark_modal/spark_modal.dart';
 import '../common/spark_widget.dart';
 
+@reflectable
+class ButtonProps {
+  // Matches:
+  // "ok:Submit[primary small]"
+  // "ok:Submit"
+  // "ok"
+  static final _SPEC_REGEXP =
+      new RegExp(r'([\w-]+)(?::([^\[\]]+)(?:\[(.+)\])?)?');
+
+  static const _STANDARD_BUTTONS = const {
+      'ok': 'OK',
+      'cancel': 'Cancel',
+      'help': 'Help',
+      'reset': 'Reset'
+  };
+
+  String text = '';
+  String firedEvent;
+
+  bool primary = false;
+  bool large = false;
+  bool small = false;
+  bool noPadding = false;
+  bool overlayToggle = false;
+  String dataDismiss = '';
+  bool focused;
+
+  ButtonProps.fromSpec(String spec) {
+    final Match m = _SPEC_REGEXP.matchAsPrefix(spec.trim());
+    firedEvent = m[1];
+    text = m[2];
+    if (text == null) {
+      text = _STANDARD_BUTTONS[firedEvent];
+    }
+    if (firedEvent == 'ok' || firedEvent == 'cancel') {
+      overlayToggle = true;
+      if (firedEvent == 'ok') {
+        primary = true;
+      }
+    }
+    // Note that explicit attributes take precedence over the defaults above.
+    if (m[3] != null) {
+      final List<String> attrs = m[3].split(' ');
+      for (final attr in attrs) {
+        switch (attr) {
+          case 'primary': primary = true; break;
+          case 'secondary': primary = false; break;
+          case 'large': large = true; break;
+          case 'small': small = true; break;
+          case 'noPadding': noPadding = true; break;
+          case 'focused': focused = true; break;
+          case 'overlay-toggle': overlayToggle = true; break;
+          default: assert(false);
+        }
+      }
+    }
+    if (overlayToggle) {
+      dataDismiss = 'modal';
+    }
+  }
+}
+
 @CustomTag('spark-dialog')
 class SparkDialog extends SparkWidget {
-  SparkModal _modal;
-
   /**
    * The title of the dialog.
    */
@@ -22,12 +82,16 @@ class SparkDialog extends SparkWidget {
    * Specially formatted specification of buttons and their roles that the
    * dialog should display.
    */
-  @published String buttons = '';
+  @published String buttons = 'cancel;ok';
 
   /**
    * The kind of animation that the overlay should perform on open/close.
    */
   @published String animation = '';
+
+  @observable final buttonProps = new ObservableList<ButtonProps>();
+
+  SparkModal _modal;
 
   SparkDialog.created() : super.created();
 
@@ -36,6 +100,10 @@ class SparkDialog extends SparkWidget {
     super.enteredView();
 
     _modal = $['modal'];
+
+    List<String> buttonSpecs = buttons.split(';');
+    buttonProps.addAll(
+        buttonSpecs.map((spec) => new ButtonProps.fromSpec(spec)));
   }
 
   void show() {
